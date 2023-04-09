ff9d511427602f5ba7b8a3f49e74ac29ada5c20f
FallOfTheHell <gleb.epifancev@mail.ru>
2023-04-09 22:46:56 +0700

    JUNIT test

--- test/com/resume/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/resume/webapp/storage/AbstractArrayStorageTest.java
@@ -1,18 +1,20 @@
 package com.resume.webapp.storage;
 
+import com.resume.webapp.exception.ExistStorageException;
 import com.resume.webapp.exception.NotExistStorageException;
+import com.resume.webapp.exception.StorageException;
 import com.resume.webapp.model.Resume;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public abstract class AbstractArrayStorageTest {
-    protected Storage storage;
-    private Resume uuid1 = new Resume("uuid1");
-    private final Resume uuid2 = new Resume("uuid2");
-    private final Resume uuid3 = new Resume("uuid3");
+    private final Storage storage;
+    private final Resume resume1 = new Resume("UUID_1");
+    private final Resume resume2 = new Resume("UUID_2");
+    private final Resume resume3 = new Resume("UUID_3");
+    private final Resume resume4 = new Resume("UUID_4");
 
     public AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
@@ -21,58 +23,105 @@ public abstract class AbstractArrayStorageTest {
     @BeforeEach
     public void setUp() {
         storage.clear();
-        storage.save(uuid1);
-        storage.save(uuid2);
-        storage.save(uuid3);
+        storage.save(resume1);
+        storage.save(resume2);
+        storage.save(resume3);
+        //storage.save(resume4);
     }
 
     @Test
     void get() {
-        assertEquals(uuid1, storage.get("uuid1"));
+        assertGet(resume1);
+    }
+
+    void assertGet(Resume resume){
+        assertEquals(resume, storage.get(resume.getUuid()));
     }
 
     @Test
     void save() {
-        assertEquals(uuid1, storage.get("uuid1"));
+        storage.save(resume4);
+        assertGet(resume4);
+        assertSize(4);
     }
 
     @Test
     void update() {
-        storage.update(uuid1);
-        assertEquals(uuid1, storage.get(String.valueOf(uuid1)));
+        storage.update(resume1);
+        assertSame(resume1, storage.get("UUID_1"));
     }
 
     @Test
     void delete() {
-        storage.delete("uuid1");
-        //assertEquals(uuid1, storage.get("uuid1"));
-        //TODO: У меня выбрасывается исключение "NullPointerException",
-        // мне нужно его отработать или я что-то делаю не так?
+        storage.delete("UUID_3");
+        assertThrows(NotExistStorageException.class, () -> {
+            assertGet(resume3);
+        });
+        //TODO: Не понимаю, ожидаю исключение NotExistStorageException,
+        // компилятор ругается на то что ожидает исключение ArrayIndexOutOfBoundsException
+        // ставишь исключение AIOFBE ожидает NESE, какой-то замкнутый круг)
+        assertSize(2);
     }
 
     @Test
     void clear() {
         storage.clear();
-        assertEquals(0, storage.size());
+        assertArrayEquals(new Resume[0], storage.getAll());
+        assertSize(0);
     }
 
     @Test
     void getAll() {
-        storage.getAll();
-        assertEquals(3, storage.size());
+        final Resume[] actual = new Resume[]{resume1, resume2, resume3};
+        assertArrayEquals(actual, storage.getAll());
     }
 
     @Test
     void size() {
-        assertEquals(3, storage.size());
+        assertSize(3);
+    }
+
+    void assertSize(int size){
+        assertEquals(size, storage.size());
     }
 
     @Test
     void getNotExist() {
-        //TODO: Я не нашел как сделать в методе ожидаемое исключение
-        // кроме как try-catch, или я плохо искал?
         assertThrows(NotExistStorageException.class, () -> {
-            storage.get("dummy");
+            storage.get("UUID_NOT_EXIST");
+        });
+    }
+
+    @Test
+    void saveExist(){
+        assertThrows(StorageException.class, () -> {
+            storage.save(resume1);
+        });
+
+        assertThrows(ExistStorageException.class, () -> {
+            storage.save(resume1);
+        });
+    }
+
+    @Test
+    void updateNotExist(){
+        assertThrows(NotExistStorageException.class, () -> {
+            storage.update(resume4);
+        });
+    }
+
+    @Test
+    void deleteNotExist(){
+        assertThrows(NotExistStorageException.class, () -> {
+            storage.delete("UUID_NOT_EXIST");
+        });
+    }
+
+    @Test
+    void saveOverflow(){
+        assertThrows(StorageException.class, () -> {
+            storage.save(resume3);
+            fail("Переполнение произошло раньше времени");
         });
     }
 }
