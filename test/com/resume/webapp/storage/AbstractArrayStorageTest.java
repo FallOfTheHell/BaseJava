915ebc68984c0d4b44eb39232c4b40620a76e6a1
FallOfTheHell <gleb.epifancev@mail.ru>
2023-04-11 21:53:32 +0700

    JUNIT test

--- test/com/resume/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/resume/webapp/storage/AbstractArrayStorageTest.java
@@ -13,17 +13,20 @@ import static org.junit.jupiter.api.Assertions.*;
 public abstract class AbstractArrayStorageTest {
     private final Storage storage;
 
-    private static final String UUID_1 = "uuid_1";
-    private static final String UUID_2 = "uuid_2";
-    private static final String UUID_3 = "uuid_3";
-    private static final String UUID_4 = "uuid_4";
     private static final String UUID_OVERFLOW = "uuid_overflow";
     private static final String UUID_NOT_EXIST = "uuid_not_exist";
-    private static final Resume resume1 = new Resume(UUID_1);
-    private static final Resume resume2 = new Resume(UUID_2);
-    private static final Resume resume3 = new Resume(UUID_3);
-    private static final Resume resume4 = new Resume(UUID_4);
 
+    private static final String UUID_1 = "uuid_1";
+    private static final Resume resume1 = new Resume(UUID_1);
+
+    private static final String UUID_2 = "uuid_2";
+    private static final Resume resume2 = new Resume(UUID_2);
+
+    private static final String UUID_3 = "uuid_3";
+    private static final Resume resume3 = new Resume(UUID_3);
+
+    private static final String UUID_4 = "uuid_4";
+    private static final Resume resume4 = new Resume(UUID_4);
 
     public AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
